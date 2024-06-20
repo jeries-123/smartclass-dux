2a937bb242cae7d728cbcfb8bbd668435820c370
jeries123 <jeriest59@gmail.com>
2024-06-20 16:40:58 +0300

    test66

--- debug.py
+++ debug.py
@@ -5,14 +5,11 @@ DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 27
 
 try:
-    platform = platform_detect.platform_detect()
-    if platform == 'Raspberry_Pi':
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        if humidity is not None and temperature is not None:
-            print(f"Temp: {temperature:.1f}C  Humidity: {humidity:.1f}%")
-        else:
-            print("Failed to retrieve data from humidity sensor")
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print(f"Temp: {temperature:.1f}C  Humidity: {humidity:.1f}%")
     else:
-        print("Unsupported platform")
+        print("Failed to retrieve data from the humidity sensor")
 except RuntimeError as e:
     print(f"RuntimeError: {e}")
+
