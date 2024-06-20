8cba0f4516e6c94587d2827ac6cd5284b7f0e9dc
jeries123 <jeriest59@gmail.com>
2024-06-20 09:52:19 +0300

    debuging the issue of some browser request

--- api.cpp
+++ api.cpp
@@ -4,6 +4,9 @@
 #include <cstring>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
 
 #define RELAY_PIN 0       // GPIO17
 #define PROJECTOR_PIN 1   // GPIO18
@@ -20,9 +23,22 @@ void handleRequest(int client_socket) {
     const int buffer_size = 2048;
     char buffer[buffer_size];
     memset(buffer, 0, buffer_size);
-    int read_size = read(client_socket, buffer, buffer_size - 1);
+    int total_read = 0;
 
-    if (read_size > 0) {
+    while (total_read < buffer_size - 1) {
+        int read_size = read(client_socket, buffer + total_read, buffer_size - 1 - total_read);
+        if (read_size < 0) {
+            std::cerr << "Error reading request: " << strerror(errno) << std::endl;
+            close(client_socket);
+            return;
+        }
+        if (read_size == 0) {
+            break;
+        }
+        total_read += read_size;
+    }
+
+    if (total_read > 0) {
         std::cout << "Received request: " << buffer << std::endl;
 
         const char* response;
@@ -47,7 +63,7 @@ void handleRequest(int client_socket) {
 
         write(client_socket, response, strlen(response));
     } else {
-        std::cerr << "Error reading request" << std::endl;
+        std::cerr << "Received empty request or error reading request" << std::endl;
     }
 
     close(client_socket);
@@ -62,9 +78,12 @@ int main() {
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
-        std::cerr << "Error opening socket" << std::endl;
+        std::cerr << "Error opening socket: " << strerror(errno) << std::endl;
         return 1;
     }
+
+    int opt = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -72,7 +91,7 @@ int main() {
     server_addr.sin_port = htons(5000);
 
     if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Error binding socket" << std::endl;
+        std::cerr << "Error binding socket: " << strerror(errno) << std::endl;
         close(server_socket);
         return 1;
     }
@@ -84,7 +103,7 @@ int main() {
     while (true) {
         int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
         if (client_socket < 0) {
-            std::cerr << "Error accepting connection" << std::endl;
+            std::cerr << "Error accepting connection: " << strerror(errno) << std::endl;
             continue;
         }
         handleRequest(client_socket);
