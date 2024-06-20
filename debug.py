e7749e27cb2a1b6f23356c05505503e28b226e7c
jeries123 <jeriest59@gmail.com>
2024-06-20 14:08:40 +0300

    debug

--- debug.py
+++ debug.py
@@ -1,10 +1,10 @@
 import Adafruit_DHT
 
-DHT_PIN = 19
+DHT_PIN = 27
 DHT_TYPE = Adafruit_DHT.DHT11
 
 try:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN, platform='Raspberry Pi')
     if humidity is not None and temperature is not None:
         print(f"Temperature: {temperature} C, Humidity: {humidity}%")
     else:
