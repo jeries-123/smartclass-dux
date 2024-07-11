c368137a358b954c14e20251c56b5d225fe4274b
jeries123 <jeriest59@gmail.com>
2024-07-11 16:14:19 +0300

    test

--- server.py
+++ server.py
@@ -115,5 +115,5 @@ def get_sensor_data():
     return jsonify(sensor_data), 200
 
 if __name__ == '__main__':
-    # Run Flask app
+    # Run Flask app using Gunicorn
     app.run(host='0.0.0.0', port=5000)
