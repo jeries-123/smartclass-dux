a52a8f0db58a902a61b5fb54dd8468960fa211d9
jeries123 <jeriest59@gmail.com>
2024-09-23 12:08:37 +0300

    cam issue

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def send_control_request(device, action):
         print(f"Error sending control request: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
